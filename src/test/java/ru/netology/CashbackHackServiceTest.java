f5a0af70277ce591122b9bf88b99c7dd2fb3ed25
Alena Katkova <alena.katkova1996@gmail.com>
2023-10-17 18:56:44 +0300

    change buid.gradle and create 4 tests

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,53 @@
+package ru.netology;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void beforeBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void thenAmountZero() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void afterBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1005;
+        int actual = service.remain(amount);
+        int expected = 995;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void afterMinusBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = -5;
+        int actual = service.remain(amount);
+        int expected = 1005;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void amountEqualsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+}
