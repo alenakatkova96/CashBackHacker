1a740a8f657ec66d2f59475715f4af5c59b49d08
Alena Katkova <alena.katkova1996@gmail.com>
2023-10-17 20:12:15 +0300

    change tests

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -42,12 +42,12 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
     }
     @Test
-    public void amountEqualsBoundary() {
+   public void amountEqualsBoundary() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 }
